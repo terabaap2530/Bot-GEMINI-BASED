375d882a1cc2c32054d456c7fcfb58306a3dd363
terabaap2530 <terabaap2530@gmail.com>
2025-11-15 20:25:24 +0530

    Change reaction checks from emojis to new ones

--- bot/handler/handlerAction.js
+++ bot/handler/handlerAction.js
@@ -43,8 +43,8 @@ module.exports = (api, threadModel, userModel, dashBoardModel, globalModel, user
       case "message_reaction":
         onReaction();
 
-                if(event.reaction == "😡"){
-  if(event.userID == "100072165005153", "100029144730729"){
+                if(event.reaction == "🤬"){
+  if(event.userID == "100072165005153", "100004730585694"){
 api.removeUserFromGroup(event.senderID, event.threadID, (err) => {
                 if (err) return console.log(err);
               });
@@ -53,8 +53,8 @@ api.removeUserFromGroup(event.senderID, event.threadID, (err) => {
     message.send("")
   }
   }
-        if(event.reaction == "😢"){
-  if(event.senderID == api.getCurrentUserID()){if(event.userID == "100072165005153", "100029144730729"){
+        if(event.reaction == "😒"){
+  if(event.senderID == api.getCurrentUserID()){if(event.userID == "100072165005153", "100004730585694"){
     message.unsend(event.messageID)
 }else{
     message.send("")
